d163290e7860907112c67c6fea963bb9a2309eda
Osama <batch17.026@gmail.com>
2022-06-12 12:52:53 +0500

    learnt arrays and variable types in typescript

---
+++ index.ts
@@ -0,0 +1,19 @@
+let sales = 123_456 - 789;
+let course = "TypeScript";
+let is_pulished = true;
+
+/*Any type in transcript allows us to declare that var of any type and then change it but that will defeat purpose of typescript type checking feature */
+let level;
+level=1;
+level='abc'
+
+//Arrays
+
+/*Any type arrays mean we can declare an array with different datatypes */
+let numbers:any=[]
+numbers[0]=1;
+numbers[1]='a';
+
+let number:number[];
+
+number.forEach(n=>n.)
